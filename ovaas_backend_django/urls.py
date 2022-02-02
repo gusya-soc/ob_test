21ef6c45401058852d5a6110ed4b3bbe96e82990
ego <18c1010016yi@edu.tech.ac.jp>
2022-02-02 14:33:16 +0000

    add:regmodel

--- ovaas_backend_django/urls.py
+++ ovaas_backend_django/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth',include('auth_parts.urls')),
-    path('models',include('models.urls'))
+    path('models/',include('models.urls'))
 ]
